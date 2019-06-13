c6c62171ed47fd27600b9ee4313dd36b65a7b132
Joseph Cillo <jcillo@Josephs-MBP.home>
2019-06-13 19:47:49 -0400

    created game logic 2

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,5 @@
+/*console.log("Up and running!");
 
-
-console.log("Up and running!");
-
-/*
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
@@ -14,19 +11,29 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [ ];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
+var checkForMatch = function(){
+	if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
+}
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
+var flipCard = function(cardId){
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
+	checkForMatch();
 
-if (cardsInPlay.length === 2){
-if (cardsInPlay[0]===cardsInPlay[1]) {
+	if (cardsInPlay.length === 2){
+	if (cardsInPlay[0]===cardsInPlay[1]) {
 	alert("You found a match!");
-}
-else {
+	}
+	else {
 	alert("Sorry, try again.");
+	}
+	}
 }
-}
+flipCard(0);
+flipCard(2);
