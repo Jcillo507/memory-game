be1504c9aec137c13e0d76347ddbf0a863ba325d
Joseph Cillo <jcillo@Josephs-MBP.home>
2019-06-13 19:04:52 -0400

    corrected game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,5 @@
 
+
 console.log("Up and running!");
 
 /*
@@ -6,7 +7,6 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
-
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardFour); */
 
@@ -22,9 +22,11 @@ var cardTwo = cards[2];
 cardsInPlay.push(cardTwo);
 console.log("User flipped king");
 
+if (cardsInPlay.length === 2){
 if (cardsInPlay[0]===cardsInPlay[1]) {
 	alert("You found a match!");
 }
 else {
 	alert("Sorry, try again.");
+}
 }
