bb00cd2793970e0624e36b7ffd02a66eebac4178
Joseph Cillo <jcillo@Josephs-MBP.home>
2019-06-16 14:23:11 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,12 +1,3 @@
-/*console.log("Up and running!");
-
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour); */
-
 var cards = [
 	{ 	
 		rank: "queen",
@@ -35,29 +26,40 @@ var cardsInPlay = [ ];
 
 var checkForMatch = function(){
 	if (cardsInPlay.length === 2){
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
-} else {
-  console.log("Sorry, try again.");
-}
-}
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+  			console.log("You found a match!");
+		} 	
+		else {
+  			console.log("Sorry, try again.");
+		}
+	}
 }
 
-var flipCard = function(cardId){
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
 	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage)
-  	console.log(cards[cardId].suit)
+	console.log(cards[cardId].cardImage);
+  	console.log(cards[cardId].suit);
 	checkForMatch();
-
+	this.setAttribute('src', cards[cardId].cardImage)
 	if (cardsInPlay.length === 2){
-	if (cardsInPlay[0]===cardsInPlay[1]) {
-	alert("You found a match!");
-	}
-	else {
-	alert("Sorry, try again.");
-	}
+		if (cardsInPlay[0]===cardsInPlay[1]) {
+			alert("You found a match!");
+		}
+		else {
+			alert("Sorry, try again.");
+		}
 	}
 }
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+    	var cardElement = document.createElement('img');
+    	cardElement.setAttribute('src', 'images/back.png');
+    	cardElement.setAttribute('data-id', i);
+    	cardElement.addEventListener('click', flipCard);
+    	document.getElementById('game-board').appendChild(cardElement);
+ 	}
+}
+
+createBoard();
